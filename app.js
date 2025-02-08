8b0da3ef4e2988d94d6154874479e3a98c3ac7d4
Ruan Sampaio <ruansampaio.works@gmail.com>
2025-02-08 12:23:15 -0300

    feat: implement function to add friends

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let inputElement = document.getElementById("amigo");
+    let nomeAmigo = inputElement.value.trim();
+
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+
+    atualizarLista();
+
+    inputElement.value = "";
+}
+
